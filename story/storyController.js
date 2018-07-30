e7e57a7804b9712b601f6f7e9777fc7e0d08622f
Glitch (oceanic-snake) <none>
2018-07-30 17:31:21 +0000

    🍬🐢 Checkpoint ./routes/index.js:1352857/1 ./routes/story.js:1352857/1727 ./story/storyController.js:1352857/459

---
+++ story/storyController.js
@@ -0,0 +1,16 @@
+//display all stories 
+exports.display_stories = function(req, res) {
+  res.send('not implemented yet');
+};
+
+//Create story 
+exports.create_story = function (req,res){
+  res.send('not implemented yet');
+};
+
+//delete story 
+exports.delete_story = function (req, res){
+  res.send('not implemented yet');
+};
+  
+  
