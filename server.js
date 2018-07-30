572b3c469bf1e5e0973ca0eccf4d1b21a3efbb66
Glitch (oceanic-snake) <none>
2018-07-30 13:12:47 +0000

    🐬😚 Checkpoint ./server.js:1352857/259 ./package.json:1352857/5497 ./routes/index.js:1352857/199 ./routes/user.js:1352857/197 ./routes/users.js:1352857/1 ./views/index.pug:1352857/66

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,14 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
 
 app.use('/', routes);
 app.use('/users', users);
