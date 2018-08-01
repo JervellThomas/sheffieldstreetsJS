d7350a9a70d0977bd437a6c1ec404abd45c11086
Glitch (oceanic-snake) <none>
2018-08-01 15:21:35 +0000

    🍥📮 Checkpoint ./server.js:1352857/110 ./views/partials/stylesheets.pug:1352857/2

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+//app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
 app.use('/', users);
 app.use('/', story);
